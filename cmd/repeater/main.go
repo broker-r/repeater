c17301b7af0ff2350224187f672afd67138ad585
broker-r <brocker.amr@gmail.com>
2025-06-16 21:56:23 +0300

    Add base handlers

--- cmd/repeater/main.go
+++ cmd/repeater/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"repeater/internal/prettylog"
+	"repeater/internal/storage"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -22,6 +24,34 @@ func main() {
 	logger := setupLogger()
 	logger.Debug("Debug mode enabled")
 	logger.Debug("Options", "options", opts)
+
+	storage := storage.NewStorage("./db.sqlite3", logger)
+
+	if opts.AddWord != "" {
+		if err := storage.AddWord(opts.AddWord); err != nil {
+			logger.Error("Error", prettylog.PrettyError(err))
+		}
+	}
+
+	if opts.List {
+		words, err := storage.GetWords()
+		if err != nil {
+			logger.Error("Error", prettylog.PrettyError(err))
+			os.Exit(1)
+		}
+		fmt.Printf("%-10s | %-15s | %-20s|\n", "NAME", "REPEAT COUNTER", "LAST REPEAT")
+		for _, word := range words {
+			fmt.Printf("%-10s | %-15d | %-20s|\n", word.Name, word.Repeat_counter, word.Last_repeat)
+		}
+	}
+
+	if opts.AddWord != "" {
+		storage.AddWord(opts.AddWord)
+	}
+
+	if opts.DeleteWord != "" {
+		storage.RemoveWord(opts.DeleteWord)
+	}
 }
 
 func setupLogger() *slog.Logger {
